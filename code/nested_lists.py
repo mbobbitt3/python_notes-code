d95c5505575a3821436e143ac18b3ce2c53e3f47
Matt Bobbitt <mattbobitt3@gmail.com>
2021-10-18 12:42:42 -0400

    in class exercises incomplete during lecture nested loops 10/18

---
+++ code/nested_lists.py
@@ -0,0 +1,13 @@
+
+students = [
+	['John', 85, 90],
+	['Lee', 85, 91],
+	['Alex', 89, 89, 95]
+]
+temp_list = students[1]
+
+for i in range(len(temp_list)):
+	print("Name: ", temp_list[i])
+	i+=1
+	print("EX1: ", temp_list[i])
+	print("EX2: ")
