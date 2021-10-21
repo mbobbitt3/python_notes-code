d7df2df2b7395dc1a08a7595f74a83d3ebc089b5
Matt Bobbitt <mattbobitt3@gmail.com>
2021-10-21 12:36:25 -0400

    in class exercises complete and more nested loops and multidimensional list work (10/21)

--- code/nested_lists.py
+++ code/nested_lists.py
@@ -1,13 +1,30 @@
+def main():
+	students = [
+		['John', 85, 'm',90],
+		['Lee', 85, 91, 'm'],
+		['Alex', 89, 89,'m', 95 ]
+	]
+	temp_list = students[0]
+	counter = 1
+	for i in students:
+		for j in i:
+			if (isinstance(j, str) and j != 'm'):
+				print('\n')
+				print("Name: ", j)
+				counter = 1
+			else:
+				print("Exam: ", counter, j)
+				counter += 1
 
-students = [
-	['John', 85, 90],
-	['Lee', 85, 91],
-	['Alex', 89, 89, 95]
-]
-temp_list = students[1]
+main()
 
+
+
+
+'''
 for i in range(len(temp_list)):
 	print("Name: ", temp_list[i])
 	i+=1
 	print("EX1: ", temp_list[i])
 	print("EX2: ")
+'''
