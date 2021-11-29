e59e90b5130f43b95a1ef6abb4daaa88134761c6
Matt Bobbitt <mattbobitt3@gmail.com>
2021-11-29 12:43:52 -0500

    dictionary working, and basic api calls 11/29

--- code/api.py
+++ code/api.py
@@ -1,6 +1,7 @@
 import requests
-url = "https://swapi.dev/api/planets/19"
+url = "https://swapi.dev/api/planets"
 resp = requests.get(url)
 data = resp.json()
-
+plan_num = input("enter a planet number: ")
+show_plan = url, '/', str(plan_num)
 print(data)
